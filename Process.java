b4c62ad0aeac435f65abeee98af6354746902d46
Erick <elopezsolis@cpp.edu>
2017-04-10 00:03:51 -0700

    Class Process was created and tested. Class Process Table has methods implemented to handle the processes. Problem is that when the method Fork is called, the old process is also modified

---
+++ Process.java
@@ -0,0 +1,106 @@
+import java.util.Stack;
+
+/**
+ * Created by Erick on 4/8/2017.
+ * Class handles a process
+ * - PID
+ * - Program
+ * - User
+ * - Status
+ * -Registers
+ */
+public class Process {
+    public Process(){
+        this.pid = 0;
+        this.program = "l";
+        this.usr = "l";
+        this.status = 1;
+        registers = new int[6];
+    }
+    public Process(int newPid, String program, String user, int status, int[] newRegisters){
+        this.pid = newPid;
+        this.program = program;
+        this.usr = user;
+        this.status = status;
+        registers = newRegisters;
+
+    }
+    String header = String.format("%3s %11s %9s %7s %10s %10s %10s %10s %10s" +
+            " %10s\n","PID","Program","User","Status","PC","SP","R0","R1","R2","R3" );
+    //Refers to the process ID - "pid"
+    private int pid;
+    //The name of the program
+    private String program;
+    //The name of the user running this program
+    private String usr;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public String getUser() {
+        return usr;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public int[] getRegisters() {
+        return registers;
+    }
+
+    public void setPid(int newPid) {
+        pid = newPid;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public void setUser(String user) {
+        this.usr = user;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setRegisters(int[] registers) {
+        this.registers = registers;
+    }
+
+    /**
+     * ***Status
+     * Refers to the current status
+     * 0-Program running
+     * 1- Process is ready
+     * 2- Process is blocked
+     */
+    private int status;
+    /**
+     * Array contains 6 varibles of type int referring to the
+     * contents of 6 registers
+     *0- pc (program counter)
+     *1- sp (stack pointer)
+     *2- r0 (register 0)
+     *3- r1 (register 1)
+     *4- r2 (register 2)
+     *5- r3 (register 3)
+     */
+    private int[] registers;
+
+    public String toString(){
+        String str;
+        str = String.format("%3d %11s %9s %7d %10s %10s %10s %10s %10s %10s",
+                this.pid,this.program,this.usr,this.status,Integer.toHexString(registers[0]),
+                Integer.toHexString(registers[1]),Integer.toHexString(registers[2]),Integer.toHexString(registers[3]),
+                Integer.toHexString(registers[4]),Integer.toHexString(registers[5]));
+        return str;
+    }
+
+}
---
+++ ProcessTable.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Created by Erick on 4/8/2017.
+ * Program implements a process table
+ */
+public class ProcessTable {
+    public static void main(String[] args){
+        ProcessTable pt = new ProcessTable();
+        System.out.println(pt.toString());
+        pt.Fork();
+        System.out.println(pt.toString());
+
+    }
+    /**
+     * Process Table
+     * Structure contains the Process Table of the CPU
+     */
+    ArrayList<Process> table = new ArrayList<Process>();
+    /**
+     * Contains the contents of the CPU
+     */
+    int[] CPU  = new int[6];
+    /**
+     * Contains the process that is running
+     */
+    Process runningProcess;
+
+    /**
+     * Keeps the number of processes running
+     */
+    private int pid;
+    public ProcessTable(){
+        int[] tempReg = {1,2,3,4,5,6};
+        pid = 0;
+        Process temp = new Process(pid,"init","root",0,tempReg);
+        table.add(temp);
+        this.CPU = temp.getRegisters();
+        this.runningProcess = temp;
+
+    }
+    public Process getRunningProcess(){
+        return runningProcess;
+    }
+    public void Fork(){
+        Process temp  = getRunningProcess();
+        temp.setStatus(4);
+        this.pid++;
+        temp.setPid(this.pid);
+        this.table.add(temp);
+
+    }
+
+    public String toString(){
+        String str = String.format("CPU:\n PC = %10s SP = %10s\n R0 = %10s R1 = %10s\n R2 = %10s R3 = %10s\n",
+                Integer.toHexString(this.CPU[0]),Integer.toHexString(this.CPU[1]),Integer.toHexString(this.CPU[2]),
+                Integer.toHexString(this.CPU[3]),Integer.toHexString(this.CPU[4]),Integer.toHexString(this.CPU[5])) ;
+        str = str + "\nProcess Table:\n" + (new Process()).header;
+        for(int i =0; i<this.table.size();i++){
+            str = str + table.get(i).toString() + "\n";
+        }
+        return str;
+    }
+
+}
